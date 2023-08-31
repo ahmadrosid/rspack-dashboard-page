c280ce2ddefbdb084d558704d7b8d8676620cd9a
ahmadrosid <alahmadrosid@gmail.com>
2023-08-31 11:24:00 +0700

    fix: refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,22 @@
-import {
-  Title,
-  Text,
-  Tab,
-  TabList,
-  TabGroup,
-  TabPanel,
-  TabPanels,
-  Card,
-} from "@tremor/react";
-import KpiCardGrid from "./components/kpi-card-grid";
-import ChartView from "./components/chart-view";
-import SalesPeopleTable from "./components/sales-people-table";
+import { Sidebar } from "./components/sidebar";
+import { Header } from "./components/header";
+import { HighlightCard } from "./components/highlight-card";
+import { TailwindIndicator } from "./components/tailwind-indicator";
+import { ReportCard } from "./components/report-card";
 
 function App() {
   return (
-    <main className="p-12">
-      <Title>Dashboard</Title>
-      <Text>My awesome saas dashboard project page.</Text>
-
-      <TabGroup className="mt-6">
-        <TabList>
-          <Tab>Overview</Tab>
-          <Tab>Detail</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <KpiCardGrid />
-            <div className="mt-6">
-              <Card>
-                <ChartView />
-              </Card>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="mt-6">
-              <Card>
-                <SalesPeopleTable />
-              </Card>
-            </div>
-          </TabPanel>
-        </TabPanels>
-      </TabGroup>
-    </main>
+    <>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <main className="p-12 flex-1 bg-gray-100 space-y-4">
+          <Header />
+          <HighlightCard />
+          <ReportCard />
+        </main>
+      </div>
+      <TailwindIndicator />
+    </>
   );
 }
 
---
+++ src/components/badge.jsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+
+export function Badge({ children, className }) {
+  return (
+    <div
+      className={clsx(
+        "rounded-full bg-blue-400 text-white text-xs flex font-medium px-2 py-1",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
